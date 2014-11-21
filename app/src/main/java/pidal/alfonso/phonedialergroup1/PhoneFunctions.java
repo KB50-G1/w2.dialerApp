b45da0f530e10f01db5ef3cc62e438ad198dd67a
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2014-11-21 15:05:37 +0100

    Added compareTo method for validating phonenumber

--- app/src/main/java/pidal/alfonso/phonedialergroup1/PhoneFunctions.java
+++ app/src/main/java/pidal/alfonso/phonedialergroup1/PhoneFunctions.java
@@ -1,10 +1,14 @@
 package pidal.alfonso.phonedialergroup1;
 
+import java.lang.Comparable;
+
 /**
  * Created by Friso on 14/11/21.
  */
-public final class PhoneFunctions {
+public final class PhoneFunctions implements Comparable<Object> {
+
     private PhoneFunctions instance;
+    private String phoneNumber;
 
     private PhoneFunctions(){}
 
@@ -12,7 +16,18 @@ public final class PhoneFunctions {
         if (instance == null) {
             instance = new PhoneFunctions();
         }
-
         return instance;
+    }
+
+    @Override
+    public int compareTo(Object otherObject) {
+        String otherNumber = (String) otherObject;
+        if(phoneNumber != otherNumber) {
+            return -1;
+        } else if(phoneNumber.equals(otherNumber)){
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
