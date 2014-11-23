48a1472f6262c9ba8f96ef7b39c19f1abe0b8a27
Alfonso Pidal Ligués <alfonsopidal@gmail.com>
2014-11-24 00:48:43 +0100

    New splash screen logo and api key
    Only 100 request per day on each key…

--- app/src/main/java/pidal/alfonso/phonedialergroup1/PhoneAPI.java
+++ app/src/main/java/pidal/alfonso/phonedialergroup1/PhoneAPI.java
@@ -34,7 +34,8 @@ public class PhoneAPI {
         StringBuilder stringBuilder = new StringBuilder();
         HttpClient client = new DefaultHttpClient();
         HttpGet httpGet = new HttpGet(url);
-        httpGet.addHeader("X-Mashape-Key", "Iszt7FIyYhmshQ6ikMVTyJz9dZXOp1MMx3Djsn5EIXUGVdfIIf");
+        httpGet.addHeader("X-Mashape-Key", "jLKJwRoc21mshGabedDbjzjRDuBnp1ksebojsn9glwQJhZhpHe");
+        // other api key: Iszt7FIyYhmshQ6ikMVTyJz9dZXOp1MMx3Djsn5EIXUGVdfIIf
 
         try {
             HttpResponse response = client.execute(httpGet);
